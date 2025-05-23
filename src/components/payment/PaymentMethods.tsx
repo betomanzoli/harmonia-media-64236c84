a13731656a7246a0dc85fc628c32d7e28ef7023f
betomanzoli <146541393+betomanzoli@users.noreply.github.com>
2025-05-23 20:42:46 -0300

    Delete src/components/payment directory

--- src/components/payment/PaymentMethods.tsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-import { CreditCard, Percent } from 'lucide-react';
-import PaymentMethodsContainer from './PaymentMethodsContainer';
-
-// Re-export the main component with the same interface
-export default PaymentMethodsContainer;
